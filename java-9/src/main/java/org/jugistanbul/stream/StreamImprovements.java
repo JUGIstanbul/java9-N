b4be6c4c09d8c304da18fa9c44403fe8842e8bf1
Taner Diler <taner.diler@gmail.com>
2020-03-30 23:45:23 +0300

    Add grouping functions to stream functions

--- java-9/src/main/java/org/jugistanbul/stream/StreamImprovements.java
+++ java-9/src/main/java/org/jugistanbul/stream/StreamImprovements.java
@@ -30,6 +30,27 @@ public class StreamImprovements {
         dateOfRelease.put("Java 14", Arrays.asList(" Pattern Matching for instanceof", "Records", "Helpful NullPointerExceptions"));
     }
 
+    public static class ListOwner
+    {
+        public String name;
+        public List<Integer> points;
+
+        public ListOwner(String name, List<Integer> points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public List<Integer> getPoints(){
+            return points;
+        }
+    }
+
     public static void main(String[] args){
 
         Stream<Integer> numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -42,11 +63,54 @@ public class StreamImprovements {
 
         System.out.println();
         IntStream.iterate(3, x -> x < 10, x -> x + 3).forEach(r -> System.out.print(r + " "));
-        System.out.println();
+        System.out.println("\n");
 
         dateOfRelease.entrySet().stream()
-                .flatMap(s -> Stream.ofNullable(s.getValue()))
-                .collect(Collectors.toList()).forEach(System.out::println);
+                     .flatMap(s -> Stream.ofNullable(s.getValue()))
+                     .collect(Collectors.toList())
+                     .forEach(System.out::println);
+        System.out.println("\n");
+
+        System.out.println("JAVA 8 - Not support Flat Mapping");
+          List<ListOwner> owners = List.of(new ListOwner("Taner", List.of(1,2,3,4)),
+                          new ListOwner("Altug", List.of(21,22,23,24)),
+                          new ListOwner("Taner", List.of(31,32,33,34)));
+
+          Map<String, List<List<Integer>>> ownerPoints =
+                          owners.stream().collect(
+                                          Collectors.groupingBy(
+                                                          ListOwner::getName,
+                                                          Collectors.mapping(
+                                                                          ListOwner::getPoints,
+                                                                          Collectors.toList())));
+        System.out.println(ownerPoints);
+
+        System.out.println("JAVA 9 - Collectors has flatMapping() method");
+          Map<String, List<Integer>> ownerPoints2 =
+                          owners.stream().collect(
+                                          Collectors.groupingBy(
+                                                          ListOwner::getName,
+                                                          Collectors.flatMapping(
+                                                                          owner -> owner.getPoints().stream(),
+                                                                          Collectors.toList())));
+       System.out.println(ownerPoints2);
+
+        System.out.println("\n");
+
+
+          List<Integer> numberList = List.of(1, 2, 3, 4, 4, 4, 4, 5, 5);
+
+        System.out.println("JAVA 8 - Not support filtering while grouping");
+          Map<Integer, Long> result = numberList.stream()
+                          .filter(val -> val > 3)
+                          .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+          System.out.println(result.toString());
+
+        System.out.println("JAVA 9 - puts entries not matched to filter also in grouping result");
+          result = numberList.stream()
+                          .collect(Collectors.groupingBy(i -> i,
+                                          Collectors.filtering(val -> val > 3, Collectors.counting())));
+          System.out.println(result.toString());
 
     }
 
