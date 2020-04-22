1bb747c7ef314af756eafc315cc80a61bc3f4e85
Hüseyin Akdoğan <hakdogan@kodcu.com>
2020-04-22 16:40:51 +0300

    Comment-line added in HelpfulNullPointer example

--- java14/src/main/java/org/jugistanbul/nullpointer/HelpfulNullPointer.java
+++ java14/src/main/java/org/jugistanbul/nullpointer/HelpfulNullPointer.java
@@ -14,7 +14,21 @@ public class HelpfulNullPointer
 
         System.out.println(car.getModel().getFactory().getName());
 
-        //You must use the -XX:+ShowCodeDetailsInExceptionMessages command-line option
-        // to enable detailed exception message feature because it's disabled by default.
+        /*
+            Before Java 14, we were getting the following message in such this situation
+
+            Exception in thread "main" java.lang.NullPointerException at
+            org.jugistanbul.nullpointer.HelpfulNullPointer.main(HelpfulNullPointer.java:15)
+
+            With Java 14 Helpful NullPointerExceptions feature, we receive the message below
+
+            Exception in thread "main" java.lang.NullPointerException:
+            Cannot invoke "org.jugistanbul.nullpointer.Factory.getName()"
+            because the return value of "org.jugistanbul.nullpointer.Model.getFactory()" is null
+            at org.jugistanbul.nullpointer.HelpfulNullPointer.main(HelpfulNullPointer.java:15)
+
+            You must use the -XX:+ShowCodeDetailsInExceptionMessages command-line option
+            to enable detailed exception message feature because it's disabled by default.
+        */
     }
 }
